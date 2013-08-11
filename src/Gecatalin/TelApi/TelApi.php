56cb92f1b921bc495f106ef10c8f75ffdbeae4e2
Cenac Catalin <catalin@neh.ro>
2013-08-11 10:31:52 +0300

    Update TelApi.php

--- src/Gecatalin/TelApi/TelApi.php
+++ src/Gecatalin/TelApi/TelApi.php
@@ -1,27 +1,27 @@
 <?php
 namespace TelApi;
-require_once __DIR__.'/../../../vendor/TelApi/telapi-php/library/TelApi/Exception.php';
+require_once __DIR__.'/../../../../telapi-php/library/TelApi/Exception.php';
 
 /** @see TelApi_Schemas */
-require_once __DIR__.'/../../../vendor/TelApi/telapi-php/library/TelApi/Schemas.php';
+require_once __DIR__.'/../../../../telapi-php/library/TelApi/Schemas.php';
 
 /** @see TelApi_InboundXML **/
-require_once __DIR__.'/../../../vendor/TelApi/telapi-php/library/TelApi/InboundXML.php';
+require_once __DIR__.'/../../../../telapi-php/library/TelApi/InboundXML.php';
 
 /** @see TelApi_Helpers **/
-require_once __DIR__.'/../../../vendor/TelApi/telapi-php/library/TelApi/Helpers.php';
+require_once __DIR__.'/../../../../telapi-php/library/TelApi/Helpers.php';
 
 /** @see TelApi_Connector **/
-require_once __DIR__.'/../../../vendor/TelApi/telapi-php/library/TelApi/Connector.php';
+require_once __DIR__.'/../../../../telapi-php/library/TelApi/Connector.php';
 
 /** @see TelApi_Related **/
-require_once __DIR__.'/../../../vendor/TelApi/telapi-php/library/TelApi/Related.php';
+require_once __DIR__.'/../../../../telapi-php/library/TelApi/Related.php';
 
 /** @see TelApi_Client **/
-require_once __DIR__.'/../../../vendor/TelApi/telapi-php/library/TelApi/Sdk.php';
+require_once __DIR__.'/../../../../telapi-php/library/TelApi/Sdk.php';
 
 /** @see TelApi_Client **/
-require_once __DIR__.'/../../../vendor/TelApi/telapi-php/library/TelApi/Connect.php';
+require_once __DIR__.'/../../../../telapi-php/library/TelApi/Connect.php';
 
 class TelApi extends \TelApi_Related{
 	public function __construct($options = null){
