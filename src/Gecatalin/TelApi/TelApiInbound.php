754ddc86bbdd1621eeb186149d723026018fc35a
Cenac Catalin <catalin@neh.ro>
2013-08-16 14:55:09 +0300

    Update TelApiInbound.php

--- src/Gecatalin/TelApi/TelApiInbound.php
+++ src/Gecatalin/TelApi/TelApiInbound.php
@@ -1,5 +1,26 @@
 <?php
 namespace Telapi;
+require_once __DIR__.'/../../../../telapi-php/library/TelApi/Exception.php';
+
+/** @see TelApi_Schemas */
+require_once __DIR__.'/../../../../telapi-php/library/TelApi/Schemas.php';
+
+/** @see TelApi_InboundXML **/
 require_once __DIR__.'/../../../../telapi-php/library/TelApi/InboundXML.php';
+
+/** @see TelApi_Helpers **/
+require_once __DIR__.'/../../../../telapi-php/library/TelApi/Helpers.php';
+
+/** @see TelApi_Connector **/
+require_once __DIR__.'/../../../../telapi-php/library/TelApi/Connector.php';
+
+/** @see TelApi_Related **/
+require_once __DIR__.'/../../../../telapi-php/library/TelApi/Related.php';
+
+/** @see TelApi_Client **/
+require_once __DIR__.'/../../../../telapi-php/library/TelApi/Sdk.php';
+
+/** @see TelApi_Client **/
+require_once __DIR__.'/../../../../telapi-php/library/TelApi/Connect.php';
 class TelApiInbound extends TelApi_InboundXML{
 }
