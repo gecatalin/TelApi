fe8c3ef9ad5029986befab063c27baf7c5163231
Cenac Catalin <catalin@neh.ro>
2013-08-16 14:51:26 +0300

    Update TelApiInbound.php

--- src/Gecatalin/TelApi/TelApiInbound.php
+++ src/Gecatalin/TelApi/TelApiInbound.php
@@ -1,5 +1,5 @@
 <?php
 namespace Telapi;
-require_once __DIR__."/../../../telapi-php/library/TelApi/InboundXML.php";
+require_once __DIR__.'/../../../../telapi-php/library/TelApi/InboundXML.php';
 class TelApiInbound extends TelApi_InboundXML{
 }
